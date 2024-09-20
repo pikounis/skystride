4ed2ecedb6416d3be04185e920efd23c7a48459d
Pallav <pallavhingu@gmail.com>
2024-09-20 16:49:59 +0100

    ADD starting first iteration of leaderboard section, needs more work

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 // pages/Home.js
 import React from 'react';
+import LeaderboardsSection from './Components/LeaderboardSection/LeaderboardSection';
 
 function Home() {
   return (
     <div className='container'>
       <h1>Welcome to the Home Page</h1>
+      <LeaderboardsSection />
     </div>
   );
 }
