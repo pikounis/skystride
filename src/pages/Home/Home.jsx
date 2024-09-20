a583e11076eadfbbc410d3e98d5efea875678e89
Pallav <pallavhingu@gmail.com>
2024-09-20 15:48:16 +0100

    ADD created two charts with mock data & adaptive sizing at 750px screen width

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,73 @@
 // pages/Home.js
 import React from 'react';
+import LineGraph from './Components/LineGraph/LineGraph';
+import styles from './Home.module.css';
+
+const mockDataName = "Jack";
+
+const mockDataPoints = [
+  {
+    name: 'Mon 9th',
+    pv: 90,
+  },
+  {
+    name: 'Tue 10th',
+    pv: 60,
+  },
+  {
+    name: 'Wed 11th',
+    pv: 70,
+  },
+  {
+    name: 'Thu 12th',
+    pv: 50,
+  },
+  {
+    name: 'Fri 13th',
+    pv: 40,
+  }
+];
+
+const mockDataHoursWorkedOut = [
+  {
+    name: 'Mon 9th',
+    pv: 10,
+  },
+  {
+    name: 'Tue 10th',
+    pv: 40,
+  },
+  {
+    name: 'Wed 11th',
+    pv: 70,
+  },
+  {
+    name: 'Thu 12th',
+    pv: 35,
+  },
+  {
+    name: 'Fri 13th',
+    pv: 40,
+  }
+];
 
 function Home() {
   return (
-    <div className='container'>
-      <h1>Welcome to the Home Page</h1>
+    <div className={styles.container}>
+      <h1>Hello {mockDataName}!</h1>
+
+      <h4>Points earned in the last 5 days</h4>
+      <div className={styles.chartsSection}>
+        <LineGraph data={mockDataPoints}/>
+        
+        <h4>Hours worked out in the last 5 days</h4>
+        <div className={styles.hoursWorkedChart}>
+          {/* Add filtering component here */}
+          <LineGraph data={mockDataHoursWorkedOut} />
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
