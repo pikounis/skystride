0d2d4cfa24762f7117bc4e7494b16dba77dcd207
Jeanette Kho <khojeanette@gmail.com>
2024-09-22 16:00:11 +0100

    ADD table component on Activity page

--- src/pages/Activity/Activity.jsx
+++ src/pages/Activity/Activity.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import ActivityTable from './components/ActivityTable';
+import { Typography } from '@mui/material';
+
+const rows = [
+  { date: '2024-09-01', exercise: 'Running', start: '08:00', finish: '08:30', totalTime: '30 min', points: 10 },
+  { date: '2024-09-02', exercise: 'Cycling', start: '09:00', finish: '09:45', totalTime: '45 min', points: 15 },
+];
 
 function Activity() {
   return (
     <div>
-      <h1>Activity Page</h1>
+      <Typography variant="h3" component="h1" align="center" m={6}>
+        My Activity
+      </Typography>
+      <ActivityTable rows={rows} />
     </div>
   );
 }
