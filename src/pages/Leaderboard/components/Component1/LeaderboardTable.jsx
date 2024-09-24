f9c995cf7ad63599f90fa7a0c58d9d19e2ed6809
Nicuu14 <nicolae.savin14@gmail.com>
2024-09-24 16:30:40 +0100

    create the title + spacing the table

--- src/pages/Leaderboard/components/Component1/LeaderboardTable.jsx
+++ src/pages/Leaderboard/components/Component1/LeaderboardTable.jsx
@@ -7,6 +7,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Box } from '@mui/material';
+import Typography from '@mui/joy/Typography';
 
 // Import the images of the team
 import team from './teamsImages/team.jpg';
@@ -28,37 +30,65 @@ const rows = [
 
 function LeaderboardTable() {
   return (
-    <TableContainer 
-      sx = {{}}
-      component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Position</TableCell>
-            <TableCell align="right">Username</TableCell>
-            <TableCell align="right">Team</TableCell>
-            <TableCell align="right">Office</TableCell>
-            <TableCell align="right">Points</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.occupiedPlace}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 }}}
-            >
-              <TableCell  align="center" component="th" scope="row">
-                {row.occupiedPlace}
-              </TableCell>
-              <TableCell align="center">{row.username}</TableCell>
-              <TableCell align="center">{<img src={row.teamEmoji} class='img-fluid' alt="team emoji"></img>}</TableCell>
-              <TableCell align="center">{row.officeLocation}</TableCell>
-              <TableCell align="center">{row.points}</TableCell>
+    <Box
+      display = 'flex'
+      justifyContent = 'center'
+      alignItems = 'center'
+      minH
+    >
+
+      <TableContainer 
+        sx = {{
+          width: '90%',
+          my: '2.5%'
+        }}
+        component={Paper}
+      >
+
+        <Typography
+          level = 'h2'
+          sx = {{ flex: '1 1 100%' }}
+          id = 'tableTitle'
+          component = 'div'
+          display = 'flex'
+          justifyContent = 'center'
+          alignItems = 'center'
+        >
+          Leaderboard
+        </Typography>
+
+        <Table sx={{ 
+          minWidth: 100,
+          }} aria-label="simple table"
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>Position</TableCell>
+              <TableCell>Username</TableCell>
+              <TableCell>Team</TableCell>
+              <TableCell>Office</TableCell>
+              <TableCell>Points</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.occupiedPlace}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 }}}
+              >
+                <TableCell  align="center" component="th" scope="row">
+                  {row.occupiedPlace}
+                </TableCell>
+                <TableCell align="center">{row.username}</TableCell>
+                <TableCell align="center">{<img src={row.teamEmoji} class='img-fluid' alt="team emoji"></img>}</TableCell>
+                <TableCell align="center">{row.officeLocation}</TableCell>
+                <TableCell align="center">{row.points}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
 
