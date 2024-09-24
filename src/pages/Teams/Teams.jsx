a45443117da3b2a2c8f3a60faf13b496d7cd337f
Anastasios Pikounis <anastasiospikounis@gmail.com>
2024-09-24 17:11:19 +0100

    start of teams component

--- src/pages/Teams/Teams.jsx
+++ src/pages/Teams/Teams.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './Teams.module.css';
+import Typography from "@mui/material/Typography";
+import RadioButtons from "./components/RadioButtons/RadioButtons";
+import TeamsSearch from './components/TeamsSearch/TeamsSearch';
+import CreateTeam from './components/CreateTeam/CreateTeam';
+import TeamCard from "./components/TeamCard/TeamCard";
 
 function Teams() {
-  return (
-    <div>
-      <h1>Teams Page</h1>
-    </div>
-  );
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = (term) => {
+        setSearchTerm(term);
+    };
+
+    const handleButtonClick = () => {
+        console.log('Button clicked!');
+    };
+
+    return (
+        <div>
+            <div className={styles.container}>
+                <Typography className={styles.title} variant="h3">Teams</Typography>
+            </div>
+            <div className={styles.controlBar}>
+                <TeamsSearch onSearch={handleSearch} />
+                <RadioButtons />
+                <CreateTeam onClick={handleButtonClick} />
+            </div>
+            <div>
+                <TeamCard/>
+            </div>
+        </div>
+    );
 }
 
 export default Teams;
