8bc5eb95dbee0207d0d1e3765772590cdec3613b
Pallav <pallavhingu@gmail.com>
2024-09-25 14:04:47 +0100

    EDIT fixed formatting, enhanced code with more MUI support

--- src/pages/Home/Components/AchievementsSection/ProgressBar.jsx
+++ src/pages/Home/Components/AchievementsSection/ProgressBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './ProgressBar.module.css'
+import { Box, Typography } from '@mui/material';
+import Proptypes from 'prop-types';
 
 // TO DO
 // add param and styling for Badge Images to put in line with text
@@ -7,18 +9,34 @@ import styles from './ProgressBar.module.css'
 // IF YOU'RE FEELING LUCKY, PUNK
 // add style={{ background: `${badgeColors}%`}} to pull unique badge colors for each badge type
 
-function ProgressBar({achievementTitle, achievementProgress, progressLeft, badgeIcon}) {
-  return (
-        <div className={styles.achievementBox}>
-            <img src={badgeIcon} className={styles.badgeIcon}/>
-            <span className={styles.title} style={{ verticalAlign: 'right'}}>{achievementTitle}</span>
-            <div className={styles.achievementBar}>
-                <span className={styles.achievementProgress} style={{ width: `${achievementProgress}%`}}>
-                    <span className={styles.pointsLeft}>{progressLeft}pts to go!</span>
-                </span>
-            </div>
-        </div>
-  )
+function ProgressBar({ achievementTitle, achievementProgress, progressLeft, badgeIcon }) {
+    return (
+        <Box sx={{
+            display: 'flex',
+            flexWrap: 'nowrap',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'ce'
+        }} className={styles.achievementBox}>
+            <img src={badgeIcon} className={styles.badgeIcon} />
+            <Box sx={{width: "100%"}}>
+                <Typography variant='h6' className={styles.title} sx={{fontWeight: 'bolder'}}>{achievementTitle}</Typography>
+                <div className={styles.achievementBar}>
+                    <Box className={styles.achievementProgress} sx={{ width: `${achievementProgress}%` }}> {/* Changed span to Box as it doesn't have text in it - correct mui container class is Box -> for this, style is changed to sx */}
+                        <Typography variant='h7' className={styles.pointsLeft}>{progressLeft}pts to go!</Typography> {/* Span changed to Typography as it's the correct text MUI component. -> Typography uses variant for text size and style so we used h7*/}
+                    </Box>
+                </div>
+            </Box>
+
+        </Box>
+    )
+}
+
+ProgressBar.propType = {
+    achievementTitle: Proptypes.string.isRequired,
+    achievementProgress: Proptypes.number.isRequired,
+    progressLeft: Proptypes.number.isRequired,
+    badgeIcon: Proptypes.string.isRequired
 }
 
 export default ProgressBar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import LeaderboardSection from './Components/LeaderboardSection/LeaderboardSection';
 import LineGraph from './Components/LineGraph/LineGraph';
 import styles from './Home.module.css';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import ProgressBar from './Components/AchievementsSection/ProgressBar';
 
 const mockDataName = "Jack";
@@ -57,7 +57,7 @@ const mockDataHoursWorkedOut = [
 const mockdataTeams = [
   {
     name: "Team 1",
-    imgPath: "", 
+    imgPath: "",
     leaderboard: [
       {
         position: 1,
@@ -74,10 +74,10 @@ const mockdataTeams = [
         imgPath: ""
       }
     ]
-  }, 
+  },
   {
     name: "Team 2",
-    imgPath: "", 
+    imgPath: "",
     leaderboard: [
       {
         position: 1,
@@ -117,51 +117,66 @@ function Home() {
   return (
     <div className={styles.container}>
       <h1>Hello {mockDataName}!</h1>
-      <Box sx={{ 
-          display: 'flex', 
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-          alignContent: 'stretch',
-          width: '98%',
-          flexWrap: 'wrap',
-          padding: '1%'
-        }}> 
-        <Box className={styles.chartsSection}>
-          <h4>Points earned in the last 5 days</h4>
-          <LineGraph data={mockDataPoints}/>
-          
-          <h4>Hours worked out in the last 5 days</h4>
-          <Box className={styles.hoursWorkedChart}>
-            {/* Add filtering component here */}
-            <LineGraph data={mockDataHoursWorkedOut} />
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignContent: 'stretch',
+        width: '98%',
+        flexWrap: 'wrap',
+        padding: '1%'
+      }}>
+
+        {/* Left Side Section */}
+        <Box className={styles.leftSection}>
+          {/* Charts Section */}
+          <Box className={styles.chartsSection}>
+            <h4>Points earned in the last 5 days</h4>
+            <LineGraph data={mockDataPoints} />
+
+            <h4>Hours worked out in the last 5 days</h4>
+            <Box className={styles.hoursWorkedChart}>
+              {/* Add filtering component here */}
+              <LineGraph data={mockDataHoursWorkedOut} />
+            </Box>
+          </Box>
+
+          {/* Achievement Section */}
+          <Box className={styles.achievementsSection}>
+            <Typography variant='h5' sx={{fontWeight: 'bolder'}}>Achievements In Progress</Typography>
+            <ProgressBar
+              achievementTitle="Novice Runner"
+              achievementProgress={90}
+              progressLeft={10}
+              badgeIcon={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM6kco1vTUHJToNzQBlwRoI1stXwneicJ1_g&s"}
+            />
+            <ProgressBar
+              achievementTitle="Master Novice Runner"
+              achievementProgress={70}
+              progressLeft={30}
+              badgeIcon={"https://png.pngtree.com/png-vector/20240723/ourlarge/pngtree-athletic-runner-vintage-badge-design-png-image_12965334.png"}
+            />
+            <ProgressBar
+              achievementTitle="Walking Wonder"
+              achievementProgress={60}
+              progressLeft={35}
+              badgeIcon={"https://img.freepik.com/premium-vector/young-girl-hiking-backpack-with-walking-stick-badge_18591-5527.jpg"}
+            />
           </Box>
         </Box>
 
+
+
+        {/* Leaderboards Section */}
         <Box className={styles.leaderboardSection}>
-          <LeaderboardSection teams={mockdataTeams}/>
+          <LeaderboardSection teams={mockdataTeams} />
         </Box>
       </Box>
 
-      <Box className={styles.achievementsSection}>
-        <h1 className={styles.achievementsTitle}>Achievements In Progress</h1>
-      
-      <ProgressBar 
-        achievementTitle="Novice Runner"
-        achievementProgress={60} 
-        progressLeft={40}
-        />
-
-    <ProgressBar 
-        achievementTitle="Master Novice Runner"
-        achievementProgress={90} 
-        progressLeft={10}
-        />
 
 
-      </Box>
-        
-      
-      
+
+
     </div>
   );
 }
