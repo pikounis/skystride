bca2e370bf9c78db9268437ce7a0d11162925a53
Pallav <pallavhingu@gmail.com>
2024-09-26 16:54:01 +0100

    removed fetch function for debugging

--- src/pages/TestPage/HealthPage.jsx
+++ src/pages/TestPage/HealthPage.jsx
@@ -2,15 +2,15 @@
 import React, { useEffect, useState } from 'react';
 
 function HealthPage() {
-    async function runGet() {
-        const response = await fetch(`http://35.176.106.162:8081/user/getAll`);
-        const json = await response.json();
-        console.log(json);
-    }
+    // async function runGet() {
+    //     const response = await fetch(`http://35.176.106.162:8081/user/getAll`);
+    //     const json = await response.json();
+    //     console.log(json);
+    // }
 
-    useEffect(() => {
-        runGet();
-    }, [])
+    // useEffect(() => {
+    //     runGet();
+    // }, [])
     return (
         <div className='container'>
             <h1>Welcome to the Test Page</h1>
