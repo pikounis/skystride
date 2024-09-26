3652d991d463002468eeda9062b2daf10c3567e8
jiya bharti <jiyabharti@outlook.com>
2024-09-26 17:48:30 +0100

    fixed the swipeable views

--- src/pages/Leaderboard/Leaderboard.jsx
+++ src/pages/Leaderboard/Leaderboard.jsx
@@ -1,102 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tabs, Tab, Box, Typography } from '@mui/material';
+import SwipeableViews from 'react-swipeable-views';
 import RankingCard from './components/Component1/RankingCard';
 import lionlogo from './Static/lionlogo.jpeg';
 import demonlogo from './Static/demonlogo.jpeg';
 import kitsunelogo from './Static/kitsunelogo.jpg';
-import { Box } from '@mui/material';
-import LeaderboardTable from './components/Component1/LeaderboardTable';
-
 
 function Leaderboard() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleSwipeChange = (index) => {
+    setValue(index);
+  };
+
   return (
     <div>
-      <h1 style={{ textAlign: 'center', color: '#6a11cb' }}>Top Teams</h1>
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="flex-end"
-        gap="20px"
-        style={{ marginTop: '50px', height: '300px' }}
-      >
-        {/* Second Place */}
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-        >
-          <Box
-            style={{
-              backgroundColor: '#c0c0c0', // Silver color for 2nd place
-              width: '200px',
-              height: '150px', // shorter for 2nd place
-              borderRadius: '10px',
-              marginBottom: '-50px', // To lift the card above the podium
-              position: 'relative',
-              zIndex: 2,
-            }}
-          />
-          <RankingCard
-            rank={2}
-            teamName="Hub Hackers"
-            points={75}
-            profileImage={demonlogo}
-          />
-        </Box>
+      {/* Tabs Header */}
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab label="Teams" />
+        <Tab label="Users" />
+        <Tab label="Offices" />
+      </Tabs>
 
-        {/* First Place */}
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-        >
-          <Box
-            style={{
-              backgroundColor: '#ffd700', // Gold color for 1st place or maybe blue?
-              width: '200px',
-              height: '200px', // taller for 1st place?
-              borderRadius: '10px',
-              marginBottom: '-50px', // positioning of the  card 
-              position: 'bottom',
-              zIndex: 3,
-            }}
-          />
-          <RankingCard
-            rank={1}
-            teamName="Sky Central Coders"
-            points={85}
-            profileImage={lionlogo}
-          />
-        </Box>
-
-        {/* Third Place */}
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-        >
-          <Box
-            style={{
-              backgroundColor: '#cd7f32', // Bronze color for 3rd place???
-              width: '200px',
-              height: '100px', // short for 3rd place
-              borderRadius: '10px',
-              marginBottom: '-50px', 
-              position: 'bottom',
-              zIndex: 1,
-            }}
-          />
-          <RankingCard
-            rank={3}
-            teamName="Pythonic Pavilion"
-            points={65}
-            profileImage={kitsunelogo}
-          />
-        </Box>
-      </Box>
-
-      <h1>Leaderboard Page</h1>
-      <LeaderboardTable />
+      {/* Swipeable Views for Tab Content */}
+      <SwipeableViews index={value} onChangeIndex={handleSwipeChange}>
         
+        {/* Tab 1: Leaderboard with Ranking Cards */}
+        <div>
+          <h1 style={{ textAlign: 'center', color: '#6a11cb' }}>Top Teams</h1>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="flex-end"
+            gap="20px"
+            style={{ marginTop: '50px', height: '300px' }}
+          >
+            {/* Second Place */}
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+            >
+              <RankingCard
+                rank={2}
+                teamName="Hub Hackers"
+                points={75}
+                profileImage={demonlogo}
+              />
+            </Box>
+
+            {/* First Place */}
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+            >
+              <RankingCard
+                rank={1}
+                teamName="Sky Central Coders"
+                points={85}
+                profileImage={lionlogo}
+              />
+            </Box>
+
+            {/* Third Place */}
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+            >
+              <RankingCard
+                rank={3}
+                teamName="Pythonic Pavilion"
+                points={65}
+                profileImage={kitsunelogo}
+              />
+            </Box>
+          </Box>
+        </div>
+
+        {/* Tab 2: Placeholder Content for "USER" */}
+        <div>
+          <Box p={3}>
+            <Typography variant="h4" align="center">
+                User 
+            </Typography>
+            <Typography align="center">
+              This is where the users leaderboard will be.
+            </Typography>
+          </Box>
+        </div>
+
+        {/* Tab 3: Placeholder Content for "ORGANISATION" */}
+        <div>
+          <Box p={3}>
+            <Typography variant="h4" align="center">
+              Offices
+            </Typography>
+            <Typography align="center">
+              This is where the offices leaderboard will be.
+            </Typography>
+          </Box>
+        </div>
+
+      </SwipeableViews>
     </div>
   );
 }
