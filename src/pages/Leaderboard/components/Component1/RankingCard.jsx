3932ee4756731215ac380e6ed3a822b560af4430
Pallav <pallavhingu@gmail.com>
2024-09-26 21:20:01 +0100

    Changed components to MUI and changed progress bar to points

--- src/pages/Leaderboard/components/Component1/RankingCard.jsx
+++ src/pages/Leaderboard/components/Component1/RankingCard.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import styles from './RankingCard.module.css';
 import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+import { Box, Typography } from '@mui/material';
 
 
 function RankingCard({ rank, teamName, points, profileImage }) {
   return (
-    <div className={styles.rankingCard}>
-      <div className={styles.rankBadge}>{rank}</div>
-      <div className={styles.cardContent}>
+    <Box className={styles.rankingCard}>
+      <Box className={styles.rankBadge}>{rank}</Box>
+      <Box className={styles.cardContent}>
         {/* <LeaderboardIcon className={styles.leaderboardIcon} /> */}
         <img src={profileImage} alt={`${teamName} logo`} className={styles.profileImage} />
-        <h2 className={styles.teamName}>{teamName}</h2>
-        <div className={styles.pointsContainer}>
+        <Typography variant='h4' className={styles.teamName}>{teamName}</Typography>
+        <Typography variant='h5' sx={{fontWeight: 'bold'}}>Points: {points}</Typography>
+        {/* <div className={styles.pointsContainer}>
           <span className={styles.pointsLabel}>Points</span>
           <div className={styles.pointsBar}>
             <div className={styles.pointsFill} style={{ width: `${points}%` }}></div>
           </div>
-        </div>
-      </div>
-    </div>
+        </div> */}
+      </Box>
+    </Box>
   );
 }
 
