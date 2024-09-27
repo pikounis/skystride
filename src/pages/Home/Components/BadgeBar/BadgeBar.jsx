d3acd48365ba1ddd488772cb666c62787e6e41c4
Tanya Waqanika <twaqanika31@gmail.com>
2024-09-27 10:59:47 +0100

    ADD added badge images and dots instead of arrows

--- src/pages/Home/Components/BadgeBar/BadgeBar.jsx
+++ src/pages/Home/Components/BadgeBar/BadgeBar.jsx
@@ -4,50 +4,73 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Badge from '../Badge/Badge.jsx';
 import Box from '@mui/material/Box';
-
+import boxingBadge from "./static/boxing-badge.png";
+import cyclingBadge from "./static/cycling-badge.png";
+import footballBadge from "./static/football-badge.png";
+import racketssportsBadge from "./static/racketssports-badge.png";
+import runningBadge from "./static/running-badge.png";
+import strengthTrainingBadge from "./static/strength-training-badge.png";
+import swimmingBadge from "./static/swimming-badge.png";
+import walkingBadge from "./static/walking-badge.png";
+import yogaBadge from "./static/yoga-badge.png";
 
 const mockBadges = 
   {
     achievementBadges: [
-    "https://png.pngtree.com/png-vector/20240723/ourlarge/pngtree-athletic-runner-vintage-badge-design-png-image_12965334.png",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM6kco1vTUHJToNzQBlwRoI1stXwneicJ1_g&s",
-    "https://img.freepik.com/premium-vector/young-girl-hiking-backpack-with-walking-stick-badge_18591-5527.jpg",
-    "https://www.tolleybadges.co.uk/wp-content/uploads/2015/12/216-blank.png",
-    "https://64.media.tumblr.com/f2033cae50aa722d209ad106b6b89f4f/tumblr_mfspw6aC0I1rha3vbo1_500.png"
-  ]
+    boxingBadge,
+    footballBadge,
+    runningBadge,
+    cyclingBadge,
+    racketssportsBadge,
+    swimmingBadge,
+    strengthTrainingBadge,
+    walkingBadge,
+    yogaBadge
+    ]
   };
 
 function BadgeBar() {
   var settings = {
     // infinite repeats badges while scrolling
-    infinite: true,
+    // infinite: true,
     speed: 500,
-    slidesToShow: 6,
-    slidesToScroll: 6,
-    initialSlide: 0,
+    dots: true,
+    centerMode: true,
+    // how many badges are shown on the badge
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    // initialSlide: 0,
     swipeToSlide: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 6,
-          slidesToScroll: 6,
-          infinite: true,
+          slidesToShow: 5,
+          dots: true,
+          centerMode: true,
+          // slidesToScroll: 1,
+          // infinite: true,
         }
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 2,
-          initialSlide: 2
+          dots: true,
+          centerMode: true,
+          slidesToScroll: 1,
+          // initialSlide: 2
+          // infinite: true
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1
+          dots: true,
+          centerMode: true,
+          slidesToScroll: 1,
+          // infinite: true
         }
       }
     ]
@@ -57,7 +80,7 @@ function BadgeBar() {
     return (
       <Box sx={{p: 2,
         boxShadow: "0 4px 2px -2px rgba(0, 0, 0, 0.2)",
-        height: '100px',
+        height: '125px',
       }}>
         <Slider {...settings}>
 
