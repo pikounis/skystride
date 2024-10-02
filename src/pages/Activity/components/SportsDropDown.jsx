387ce33b3c33e9c299dd54c544f329030c4c011c
Tanya Waqanika <twaqanika31@gmail.com>
2024-10-02 23:05:51 +0100

    added sportsdropdown code

--- src/pages/Activity/components/SportsDropDown.jsx
+++ src/pages/Activity/components/SportsDropDown.jsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Autocomplete, TextField, Box } from '@mui/material';
 import styles from '../Activity.module.css';
+import axios from 'axios';
+import { APIPath } from '../../../util';
 
 const SportsDropDown = ({ sportsData, selectedExercise }) => {
   const [selectedSport, setSelectedSport] = useState(selectedExercise);  // Initialise with selectedExercise
 
   // Update selectedSport if selectedExercise prop changes
   useEffect(() => {
+    axios.get(APIPath + "/sport/getAll");
     setSelectedSport(selectedExercise);
   }, [selectedExercise]);
+
 
   return (
     <div>
@@ -16,7 +20,7 @@ const SportsDropDown = ({ sportsData, selectedExercise }) => {
       <Box className={styles.sportsDropDown}>
         <Autocomplete
           options={sportsData}  // Options for the sports dropdown
-          getOptionLabel={(option) => option}
+          getOptionLabel={(option) => option.name}
           value={selectedSport}
           onChange={(event, value) => setSelectedSport(value)}
           renderInput={(params) => <TextField {...params} label="Select Sport" variant="outlined" />} 
